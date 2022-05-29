ecbfd119da290f4434fcba6ca9d45336d5ae0c76
Nils Berghs <nbe@delta-engineering.be>
2022-05-29 17:20:35 +0200

    Very basic implementation for session totals

--- EetFestijnLib/ViewModels/SessionViewModel.cs
+++ EetFestijnLib/ViewModels/SessionViewModel.cs
@@ -16,7 +16,29 @@ namespace be.berghs.nils.EetFestijnLib.ViewModels
         
         public OrderViewModel CurrentOrder { get; }
 
-        public OrderList OrderList { get; }
+        private int _OrderCount;
+        public int OrderCount 
+        { 
+            get => _OrderCount; 
+            private set => SetProperty(ref _OrderCount, value);
+        }
+
+        private int _PlateCount;
+        public int PlateCount
+        {
+            get => _PlateCount;
+            private set => SetProperty(ref _PlateCount, value);
+        }
+
+        private decimal _TotalIncome;
+        /// <summary>
+        /// The theoretical income, vouchers included
+        /// </summary>
+        public decimal TotalIncome
+        {
+            get => _TotalIncome;
+            private set => SetProperty(ref _TotalIncome, value);
+        }
 
         public SessionViewModel(StackViewModel<PageViewModel> stackViewModel, IDialogService dialogService, Session session):base(stackViewModel, dialogService)
         {
@@ -25,15 +47,36 @@ namespace be.berghs.nils.EetFestijnLib.ViewModels
             Session.OrderList.OrderAdded += OrderListOrderAdded;
                         
             CurrentOrder = new OrderViewModel(dialogService, Session);
+
+            //recalculate the totals, this is usefull when opening an existing session
+            RecalculateTotals();
         }
 
        
 
         private void OrderListOrderAdded(object sender, OrderAddedEventArgs e)
         {
-            //Todo recalc everything
+            RecalculateTotals();
+
+            //Todo save order to temp file async
         }
 
+        private void RecalculateTotals()
+        {
+            int orderCount = 0;
+            int plateCount = 0;
+            decimal totalIncome = 0;
+            foreach (var order in Session.OrderList.Orders)
+            {
+                orderCount++;
+                totalIncome += order.TotalPrice;
+                foreach (var item in order.Foods)
+                    plateCount += item.Count;
+            }
 
+            OrderCount = orderCount;
+            PlateCount = plateCount;
+            TotalIncome = totalIncome;
+        }
     }
 }
